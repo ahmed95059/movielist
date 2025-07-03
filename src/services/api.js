0a5b368fe409c5fc4ebae8606fc3ff302af816bc
ahmed95059 <ahmed.bayachatti@supcom.tn>
2025-07-04 00:20:49 +0200

    first commit

---
+++ src/services/api.js
@@ -0,0 +1,14 @@
+const API_KEY = "84e1f73dd2cc7bbd78405b923619f25b"
+const BASE_URL= "https://api.themoviedb.org/3"
+export const getPopularMovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+
+};
+export const searchMovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json();
+    return data.results;
+    
+};
